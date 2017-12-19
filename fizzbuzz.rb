8588d8df828aa98fc6980d91982fa5bbc9e7314b
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-19 17:11:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,8 @@ def fizzbuzz(int)
     if int % 3 == 0
       puts "Fizz"
     end
-    elseif int % 5 == 0
+  elsif int % 5 == 0
+     int % 5 == 0
       puts "Buzz"
     end
   else int % 3 != 0 && int % 5 != 0
